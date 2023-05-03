8a18ef5502d1355c27bee47ef242869f5a21ddc2
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2023-05-03 10:49:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const input = [1, 2, 3, 4];
+
+function myPromise(arr) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			let result = document.getElementById('output');
+			setTimeout(() => {
+				result.innerHTML = `${arr.filter(num => num % 2 != 0)}`;
+			}, 1000);
+			setTimeout(() => {
+				result.innerHTML = `${arr.map(num => {
+					if(num % 2 == 0){
+						return num * 2;
+					}
+					return num;
+				})}`;
+			}, 2000);
+		}, 3000);
+	});
+}
+myPromise(input);
